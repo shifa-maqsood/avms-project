96b4c8b266688b727923633d2a1b3d4c2541b4e1
shifa-maqsood <77151360+shifa-maqsood@users.noreply.github.com>
2021-01-08 16:56:27 +0530

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,43 @@
+<aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+        
+                   <h1>AVMS</h1>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li>
+                            <a href="dashboard.php"  style="color: blue">
+                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                        </li>
+
+     <li>
+                            <a href="visitors-form.php"  style="color: blue">
+                                <i class="fa fa-user"></i>New Visitor</a>
+                        </li>
+   <li>
+                            <a href="manage-newvisitors.php"  style="color: blue">
+                                <i class="fa fa-users"></i>Manage Visitors</a>
+                        </li>
+
+                        
+                      <li>
+                            <a href="bwdates-reports.php"  style="color: blue">
+                                <i class="fas fa-copy"></i>Vistors B/w Dates</a>
+                        </li> 
+                         
+                          <!-- enhance/shifa-->
+                          <li>
+                            <a href="House-Holders.php"  style="color: blue">  
+                                <i class="fas fa-copy"></i>House Holders Details</a>   
+                        </li> 
+                        <!--enhance/shifa-->
+                        <li>
+                            <a href="maintenance.php"  style="color: blue">
+                                <i class="fas fa-copy"></i>Maintenance Staff Details</a>   
+                        </li> 
+                       
+                    </ul>
+                </nav>
+            </div>
+        </aside>
